a1748419e534af584281fa5ffb2d9fe9719fbefc
PriyeshRai369 <priyeshrai369@gmail.com>
2024-12-18 16:02:50 +0530

    Update custom blog page.

--- src/app/WBlog/BlogForm.js
+++ src/app/WBlog/BlogForm.js
@@ -1,70 +1,151 @@
-'use client'
-import React, { useState } from 'react';
-import axios from 'axios';
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function BlogForm() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+  const [customerDetails, setCustomerDetails] = useState({
+    name: "",
+    email: "",
+    phoneNo: "",
+    nationality: "",
+    tourDetail: "",
+  });
 
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCustomerDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: value,
+    }));
+  };
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleMessageChange = (e) => {
-        setMessage(e.target.value);
-    };
+    try {
+      const response = await axios.post(
+        "https://www.earthyhues.co.in/api-custom-tour",
+        customerDetails
+      );
 
-    const handleOnSubmit = async (e) => {
-        e.preventDefault();
-
-        const data = {
-            name: name,
-            email: email,
-            message: message
-        };
-
-        try {
-            const response = await axios.post('url', data);
-            console.log(response.data);
-
-            setName('');
-            setEmail('');
-            setMessage('');
-        } catch (error) {
-            console.error('Error submitting form:', error);
-        }
-    };
-    return (
-        <form className="form-one row gutter-20" onSubmit={handleOnSubmit}>
-            <div className="col-md-6 wow animated fadeInUp" data-wow-delay="0s" data-wow-duration="1500ms">
-                <div className="form-one__group">
-                    <input type="text" name="name" placeholder="Your Name" className="form-one__input" value={name} onChange={handleNameChange} />
-                </div>
-            </div>
-            <div className="col-md-6 wow animated fadeInUp" data-wow-delay="0.3s" data-wow-duration="1500ms">
-                <div className="form-one__group">
-                    <input type="email" name="email" placeholder="Email Address" className="form-one__input" value={email} onChange={handleEmailChange} />
-                </div>
-            </div>
-            <div className="col-12 wow animated fadeInUp" data-wow-delay="0.1s" data-wow-duration="1500ms">
-                <div className="form-one__group">
-                    <textarea name="message" cols={30} rows={10} placeholder="Write a Message" className="form-one__message form-one__input" value={message} onChange={handleMessageChange} />
-                </div>
-            </div>
-            <div className="col-12 wow animated fadeInUp" data-wow-delay="0.2s" data-wow-duration="1500ms">
-                <div className="form-one__btn-box">
-                    <button type="submit" className="form-one__btn trevlo-btn trevlo-btn--base">
-                        <span>Leave a Comment</span>
-                    </button>
-                </div>
-            </div>
-        </form>
-    )
+      if (response.data[0].status == 1) {
+        toast.success(
+          "Your details have been successfully submitted! Our team will be in touch with you shortly."
+        );
+        setCustomerDetails({
+          name: "",
+          email: "",
+          phoneNo: "",
+          nationality: "",
+          tourDetail: "",
+        });
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+  return (
+    <form className="form-one row gutter-20" onSubmit={handleSubmit}>
+      <ToastContainer />
+      <div
+        className="col-md-6 wow animated fadeInUp"
+        data-wow-delay="0s"
+        data-wow-duration="1500ms"
+      >
+        <div className="form-one__group">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            className="form-one__input"
+            value={customerDetails.name}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div
+        className="col-md-6 wow animated fadeInUp"
+        data-wow-delay="0.3s"
+        data-wow-duration="1500ms"
+      >
+        <div className="form-one__group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            className="form-one__input"
+            value={customerDetails.email}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div
+        className="col-md-6 wow animated fadeInUp"
+        data-wow-delay="0.3s"
+        data-wow-duration="1500ms"
+      >
+        <div className="form-one__group">
+          <input
+            type="tel"
+            name="phoneNo"
+            placeholder="Contact No"
+            className="form-one__input"
+            value={customerDetails.phoneNo}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div
+        className="col-md-6 wow animated fadeInUp"
+        data-wow-delay="0.3s"
+        data-wow-duration="1500ms"
+      >
+        <div className="form-one__group">
+          <input
+            type="text"
+            name="nationality"
+            placeholder="Nationality"
+            className="form-one__input"
+            value={customerDetails.nationality}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div
+        className="col-12 wow animated fadeInUp"
+        data-wow-delay="0.1s"
+        data-wow-duration="1500ms"
+      >
+        <div className="form-one__group">
+          <textarea
+            name="tourDetail"
+            cols={30}
+            rows={10}
+            placeholder="Describe your details"
+            className="form-one__message form-one__input"
+            value={customerDetails.tourDetail}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div
+        className="col-12 wow animated fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="1500ms"
+      >
+        <div className="form-one__btn-box">
+          <button
+            type="submit"
+            className="form-one__btn trevlo-btn trevlo-btn--base"
+          >
+            <span>Send Request</span>
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
-export default BlogForm
+export default BlogForm;
--- src/app/WBlog/page.js
+++ src/app/WBlog/page.js
@@ -1,43 +1,47 @@
 import BlogForm from "./BlogForm";
 
 const WBlog = () => {
+  return (
+    <>
+      <section className="page-header">
+        <div className="container"></div>
+      </section>
 
-    return (
-
-        <>
-            <section className="page-header">
-                <div className="container">
-
-                </div>
-            </section>
-
-            <div className='container'>
-                        <h3
-                            className="offer-one__heading sec-title__heading text-left headingdest"
-                            style={{
-                                // marginTop: "-62px",
-                                paddingLeft: '200',
-                                fontSize: "25px!important",
-                                left: 23
-                            }}>
-                            <span className="font-bernadette-rough display-4">
-                                Write A Blog
-                            </span>
-                        </h3>
-                <div className="comment-form">
-                    <div className="comment-form__inner-container container-fluid">
-                        <h3 className="comment-form__title">Leave a Comment</h3>
-                        <BlogForm />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+      <div className="container">
+        <h3
+          className="offer-one__heading sec-title__heading text-left headingdest"
+          style={{
+            // marginTop: "-62px",
+            paddingLeft: "200",
+            fontSize: "25px!important",
+            left: 23,
+          }}
+        >
+          <span className="font-bernadette-rough display-4">Custom Tour</span>
+        </h3>
+        <div className="comment-form">
+          <div className="comment-form__inner-container container-fluid">
+            <h3 className="comment-form__title">
+              Let us Customize your Vacation. Fill up the details.
+            </h3>
+            <p style={{maxWidth:"700px",lineHeight:"1"}}>
+              At Earthy Hues we allow you to create an entirely unique travel
+              experience, designed and based around your own specific needs and
+              wishes. You choose when and where you want to travel, the duration
+              of your holiday and your personal budget.
+            </p>
+            <BlogForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WBlog;
 
 export const metadata = {
-    title: 'Write A blog about your journey and about Earthy Huse | Earthy Hues',
-    description: 'Team of travel enthusiasts with extensive knowledge of wildlife & nature tourism',
-}
+  title: "Write A blog about your journey and about Earthy Huse | Earthy Hues",
+  description:
+    "Team of travel enthusiasts with extensive knowledge of wildlife & nature tourism",
+};
