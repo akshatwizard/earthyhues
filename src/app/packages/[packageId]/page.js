edf1c65ffcba71b0f635cedb4f6c3f3812bc2da1
akshatwizard <akshat@wizards.co.in>
2025-12-03 11:16:44 +0530

    Update page.js

--- src/app/packages/[packageId]/page.js
+++ src/app/packages/[packageId]/page.js
@@ -13,7 +13,7 @@ function page({params}) {
 }
 
 export async function generateMetadata({ params }) {
-    const response = await axios.get(`https://www.earthyhues.co.in/apipackages/${params.packageId}`)
+    const response = await axios.get(`https://www.exportleftovers.in/apipackages/${params.packageId}`)
     const dt = response.data[0]
     return {
       title: `${dt.package_title} Travel Package by EarthyHues`,
